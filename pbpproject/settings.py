bebb424bb8f34e83654010a947bd35e44dcc608f
Norbert Lupamo <91078038+lupamo3@users.noreply.github.com>
2023-03-11 13:06:22 +0300

    Initial commit, includes a skeleton of the project *Pre-commit configuration Docker config in the compose and dockerfile unit tests to test search, sort and file creation model serializers Views plus data insertion to DB File models Database configuration

--- pbpproject/settings.py
+++ pbpproject/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = "pbpproject.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rest_api_db',
+        'USER': 'rest_api_user',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '5432',
+}
 }
 
 
--- pbpstart/serializers.py
+++ pbpstart/serializers.py
@@ -1,9 +1,15 @@
 # rest_api_app/serializers.py
 
 from rest_framework import serializers
-from .models import File
+from .models import File, User
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = '__all__'
+        fields = ['id', 'file', 'status', 'created_at']
+        read_only_fields = ['status', 'created_at']
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'national_id', 'birth_date', 'address', 'country', 'phone_number', 'email', 'finger_print_signature', 'file']
